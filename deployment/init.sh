ce6c6abeef1b6bfc0f9e04fee2d3806f1d3506f7
Viacheslav Davydov <dv0vd@dv0vd.xyz>
2024-12-31 09:57:43 +0300

    Refactored init file

--- deployment/init.sh
+++ deployment/init.sh
@@ -6,17 +6,11 @@ apt update &&
 apt upgrade &&
 apt install -y make &&
 apt install -y git && 
-#apt install -y net-tools &&
-#apt install -y cron &&
 
 #ssh
 cat /root/dv0vd.xyz/deployment/configs/linux/ssh.pub >> /root/.ssh/authorized_keys &&
 echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config &&
 echo 'Port 60000' >> /etc/ssh/sshd_config &&
-
-# ipv6
-#sed -i '/GRUB_CMDLINE_LINUX=/ s/"$/ ipv6.disable=1"/g' /etc/default/grub &&
-#update-grub &&
 
 # fail2ban
 apt install fail2ban -y &&
@@ -39,84 +33,5 @@ echo 'make schedule-midnight-reboot' >> /etc/rc.local &&
 echo 'exit 0' >> /etc/rc.local &&
 chmod a+x /etc/rc.local &&
 
-
-# ufw
-#apt install ufw &&
-#ufw default deny incoming &&
-#ufw default deny outgoing &&
-#ufw default allow routed &&
-#ufw allow 60000 &&
-#ufw enable
-
-#apt install ufw &&
-#ufw default allow incoming &&
-#ufw default allow outgoing &&
-#ufw default allow routed &&
-#ufw allow 60000 &&
-#ufw deny from any &&
-#ufw deny out to any &&
-#ufw enable &&
-
-
-# finish
 reboot
 
-
-
-
-
-#ufw allow openvpn &&
-
-
-
-# new user
-#userdel vps;
-#adduser vps &&
-#usermod -a -G sudo vps &&
-
-#ssh
-#mkdir /home/vps/.ssh &&
-#chown vps:vps /home/vps/.ssh -R &&
-#chmod 700 /home/vps/.ssh &&
-#chmod 600 /home/vps/.ssh/authorized_keys &&
-
-
-
-#--privileged 
-
-
-#echo 'DenyUsers root' >> /etc/ssh/sshd_config &&
-#    --cap-add=net_admin,net_raw \
-#adduser slava --disabled-password --gecos '' &&
-#usermod -a -G sudo slava &&
-#echo 'slava:fRwwiXY7L3P44oZEATkvtz7yrqIYP9ku' | chpasswd &&
-#userdel--remove  slava
-#groupdel  slava
-
-#cat ~/.ssh/vps.pub >> /home/slava/.ssh/authorized_keys &&
-#ssh-copy-id -i ~/.ssh/id_rsa.pub slava@localhost
-
-#cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
-#fail2ban-client unban --all
-
-
-
-# openvpn
-# mkdir /root/_data/openvpn -p &&
-# mkdir /root/_data/openvpn/certificates &&
-# podman load < /root/dv0vd.xyz/deployment/openvpn/kylemanna-openvpn_edge.tar &&
-#podman run \
-#    -v /root/_data/openvpn:/etc/openvpn \
-#    --name=ovpn_genconfig \
-#    docker.io/kylemanna/openvpn:edge \
-#    ovpn_genconfig -u udp://dv0vd.xyz &&
-#podman run \
-#    -v /root/_data/openvpn:/etc/openvpn \
-#    --name=ovpn_initpki \
-#    -it \
-#    docker.io/kylemanna/openvpn:edge \
-#    ovpn_initpki &&
-#podman stop ovpn_genconfig;
-#podman stop ovpn_initpki;
-#podman rm ovpn_genconfig; 
-#podman rm ovpn_initpki; 
