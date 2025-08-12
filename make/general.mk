7be66ab0ea813e2bfc8c359f83bc7f299d453854
Viacheslav Davydov <dv0vd@dv0vd.xyz>
2025-08-12 21:25:29 +0300

    Updated https-proxy version to 1.0.5

--- make/general.mk
+++ make/general.mk
@@ -22,7 +22,7 @@ podman-create-network:
 podman-load-images:
 	podman load < ./deployment/images/dv0vd-socks4_1.0.5.tar
 	podman load < ./deployment/images/dv0vd-socks5_1.0.11.tar
-	podman load < ./deployment/images/dv0vd-https-proxy_1.0.4.tar
+	podman load < ./deployment/images/dv0vd-https-proxy_1.0.5.tar
 	podman load < ./deployment/images/mongo_7.0.16.tar
 	podman load < ./deployment/images/nginx_1.27.3.tar
 	podman load < ./deployment/images/node_20.18.1-bookworm.tar
