475080dc7bc82838583097a711bff1b25170f8fb
Viacheslav Davydov <dv0vd@dv0vd.xyz>
2025-08-20 18:05:33 +0300

    Cgroup comment for future

--- deployment/init.sh
+++ deployment/init.sh
@@ -40,6 +40,7 @@ podman system prune --all -f &&
 cgcreate -g memory,cpu:/podman-group &&
 echo $((512*1024*1024)) | sudo tee /sys/fs/cgroup/mygroup/memory.max &&
 echo "50000 100000" | sudo tee /sys/fs/cgroup/mygroup/cpu.max && # quota period, period = 100000 ms
+# systemctl set-property podman-group.slice MemoryMax=512M CPUQuota=50% ???
 
 # restart
 rm /etc/rc.local -f &&
