364bf6bb7770b6f8313e6f0610550bbb3dfd9ff1
Viacheslav Davydov <dv0vd@dv0vd.xyz>
2025-09-14 19:06:46 +0300

    Git problems

--- make/start.mk
+++ make/start.mk
@@ -96,22 +96,6 @@ start-nginx:
 # 	--cpus=${NGINX_CPUS} \
 # 	docker.io/nginx:1.27.3
 
-start-pihole:
-	- podman run \
-		-d \
-		--name pihole \
-		--network host \
-		-e TZ=UTC \
-		-e FTLCONF_dns_upstreams='1.1.1.1;8.8.8.8' \
-		-e FTLCONF_webserver_api_password=${PIHOLE_ADMIN_PASSWORD} \
-		-e FTLCONF_dns_listeningMode=all \
-		-v ./deployment/data/pihole/data:/etc/pihole \
-		--restart unless-stopped \
-		--memory=${PIHOLE_MEMORY} \
-		--cpus=${PIHOLE_CPUS} \
-		--cgroup-parent=/podman-group.slice \
-		docker.io/pihole/pihole:2025.08.0
-
 start-mongo-demo:
 	- podman run \
 	-d \
