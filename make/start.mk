a6ccaa53e89918a26eb74bc7548b8e90fa356f66
Viacheslav Davydov <dv0vd@dv0vd.xyz>
2025-09-14 14:32:17 +0300

    Added por forwarding for pihole

--- make/start.mk
+++ make/start.mk
@@ -102,6 +102,7 @@ start-pihole:
 		-d \
 		--name pihole \
 		--network podman_network \
+		-p 53:53 \
 		-e TZ=UTC \
 		-e FTLCONF_dns_upstreams='1.1.1.1;8.8.8.8' \
 		-e FTLCONF_webserver_api_password=${PIHOLE_ADMIN_PASSWORD} \
