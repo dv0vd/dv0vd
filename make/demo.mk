92d555d7c09be97b8e9ad7bc5b734b611c55ba54
Viacheslav Davydov <dv0vd@dv0vd.xyz>
2025-09-22 21:58:38 +0300

    Fixed demo repositories

--- make/demo.mk
+++ make/demo.mk
@@ -4,17 +4,17 @@ init-demo:
 
 init-timers:
 	mkdir ./demo
-	cd ./demo && git clone git@github.com:dv0vd/demo-timers.git && cp ../.env ./demo-timers/.env
+	cd ./demo && git clone https://github.com/dv0vd/demo-timers.git && cp ../.env ./demo-timers/.env
 	cd .. && podman build . -f ./deployment/configs/demo/timers.containerfile -t demo-timers
 
 init-skillnotes:
 	mkdir ./demo
-	cd ./demo && git clone git@github.com:dv0vd/demo-skillnotes.git && cp ../.env ./demo-skillnotes/.env
+	cd ./demo && git clone https://github.com/dv0vd/demo-skillnotes.git && cp ../.env ./demo-skillnotes/.env
 	cd .. && podman build . -f ./deployment/configs/demo/skillnotes.containerfile -t demo-skillnotes
 
 init-lagoona:
 	mkdir ./demo
-	cd ./demo && git clone git@github.com:dv0vd/demo-lagoona.git
+	cd ./demo && git clone https://github.com/dv0vd/demo-lagoona.git
 
 refresh-demo:
 	rm ./demo -rf
