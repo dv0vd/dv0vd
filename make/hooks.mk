77763c0e501e2dc9842f916ea3a7adb5b703cf11
Viacheslav Davydov <dv0vd@dv0vd.xyz>
2025-11-13 21:53:57 +0300

    Fixed iptables config path

--- make/hooks.mk
+++ make/hooks.mk
@@ -1,7 +1,7 @@
 on-startup:
 	- systemctl disable fail2ban
-	- bash -c "set -a; . .env; set +a; envsubst < ./deployment/iptables/iptables_lite.sh > ./deployment/iptables/iptables.sh"
-	- ./deployment/iptables/iptables.sh
+	- bash -c "set -a; . .env; set +a; envsubst < ./deployment/configs/iptables/iptables_lite.sh > ./deployment/configs/iptables/iptables.sh"
+	- ./deployment/configs/iptables/iptables.sh
 	- shutdown -r 0:00
 # - chmod +x ./deployment/configs/linux/iptables.sh && ./deployment/configs/linux/iptables.sh
 	- $(MAKE) logs-clear
