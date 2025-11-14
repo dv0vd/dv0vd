1cf30f949cee894d9c2f2558ca17f604fe7c5b4b
Viacheslav Davydov <dv0vd@dv0vd.xyz>
2025-11-14 06:48:55 +0300

    Fixed rules order

--- deployment/configs/iptables/iptables_lite.sh
+++ deployment/configs/iptables/iptables_lite.sh
@@ -8,6 +8,16 @@ iptables -P INPUT DROP
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD DROP
 
+# allow localhost
+iptables -A INPUT  -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+# drop invalid packages
+iptables -A INPUT  -m state --state INVALID -j DROP
+iptables -A OUTPUT -m state --state INVALID -j DROP
+
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT # allow established/related
+
 # block ips
 ipset create blocked_hosts iphash -exist # no error if set already exists
 ipset -F blocked_hosts # clean set
@@ -16,10 +26,6 @@ for ip in $(cat ./deployment/configs/iptables/blocked_hosts.txt); do
 done
 iptables -A INPUT -p tcp -m set --match-set blocked_hosts src -j REJECT --reject-with tcp-reset
 iptables -A INPUT -p udp -m set --match-set blocked_hosts src -j REJECT --reject-with icmp-port-unreachable
-
-# allow localhost
-iptables -A INPUT  -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
 
 # allow icmp
 iptables -A INPUT -p icmp --icmp-type 8 -m limit --limit 10/s -j ACCEPT # echo request with limit for the whole server
@@ -35,11 +41,6 @@ iptables -A INPUT -p tcp --dport $SOCKS5_PORT -j ACCEPT # allow socks5
 iptables -A INPUT -p tcp --dport $OUTLINE_PORT -j ACCEPT # allow outline
 
 # default rules
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
 iptables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
 iptables -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
-
-# drop invalid packages
-iptables -A INPUT  -m state --state INVALID -j DROP
-iptables -A OUTPUT -m state --state INVALID -j DROP
-
