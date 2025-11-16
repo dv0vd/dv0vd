76ac5cc029c10c7ee91bac2e0936d123a67bd510
Viacheslav Davydov <dv0vd@dv0vd.xyz>
2025-11-16 15:07:17 +0300

    Enabled iptables startup

--- make/hooks.mk
+++ make/hooks.mk
@@ -1,7 +1,7 @@
 on-startup:
 	- systemctl disable fail2ban
 	- bash -c 'set -a; . .env; set +a; envsubst "\$$SSH_PORT \$$SOCKS4_PORT \$$SOCKS5_PORT \$$OUTLINE_PORT" < ./deployment/configs/iptables/iptables_lite.sh > ./deployment/configs/iptables/iptables.sh'
-# - chmod +x ./deployment/configs/iptables/iptables.sh && ./deployment/configs/iptables/iptables.sh
+	- chmod +x ./deployment/configs/iptables/iptables.sh && ./deployment/configs/iptables/iptables.sh
 	- shutdown -r 0:00
 	- $(MAKE) logs-clear
 	- $(MAKE) restart-containers
