9e0bf05249a990f0908761e081a8c5c9b4585d5d
Viacheslav Davydov <dv0vd@dv0vd.xyz>
2025-11-17 21:08:37 +0300

    Merge conflicts

--- make/stop.mk
+++ make/stop.mk
@@ -1,9 +1,13 @@
 stop-containers:
 	- $(MAKE) stop-nginx
+	- $(MAKE) stop-demo
 	- $(MAKE) stop-socks4
-	- $(MAKE) stop-https-proxy
-	- $(MAKE) stop-pihole
+	- $(MAKE) stop-socks5
 	- $(MAKE) stop-outline
+	- $(MAKE) stop-https-proxy
+	- $(MAKE) stop-coturn
+	- $(MAKE) stop-synapse
+	- $(MAKE) stop-db
 
 stop-socks4:
 	- podman stop socks4
