b16a3b2795ac89815b036694edfa211d0b824f8c
Viacheslav Davydov <dv0vd@dv0vd.xyz>
2026-02-15 17:41:04 +0300

    Fixed certbot initialization

--- make/common.mk
+++ make/common.mk
@@ -18,13 +18,38 @@ issue-letsencrypt-certificate:
 		--dns ${DNS2} \
 		--dns 1.1.1.1 \
 		--dns 8.8.8.8 \
-		-v ./deployment/data/letsencrypt:/etc/letsencrypt \
+		-v ./deployment/data/letsencrypt/data:/etc/letsencrypt \
+		-v ./deployment/data/letsencrypt/acme:/app/acme \
 		docker.io/certbot/certbot:v5.3.1 certonly \
  		--webroot \
-		--webroot-path=/app/letsencrypt \
+		--webroot-path=/app/acme \
 		-d dv0vd.dev \
 		-d www.dv0vd.dev \
 		--email postmaster@dv0vd.dev \
 		--agree-tos \
 		--no-eff-email
 
+
+
+# 	- podman run \
+# 		-d \
+# 		--rm \
+# 		--name certbot \
+# 		--network podman_network \
+# 		-v ./deployment/data/ntfy:/var/lib/ntfy \
+# 		-e NTFY_BASE_URL=https://ntfy.dv0vd.dev \
+# 		-e NTFY_CACHE_FILE=/var/lib/ntfy/cache.db \
+# 		-e NTFY_AUTH_FILE=/var/lib/ntfy/auth.db \
+# 		-e NTFY_AUTH_DEFAULT_ACCESS=read-write \
+# 		-e NTFY_BEHIND_PROXY=yes \
+# 		-e NTFY_ATTACHMENT_CACHE_DIR=/var/lib/ntfy/attachments \
+# 		-e NTFY_ENABLE_LOGIN=false \
+# 		--dns ${DNS1} \
+# 		--dns ${DNS2} \
+# 		--dns 1.1.1.1 \
+# 		--dns 8.8.8.8 \
+# 		--restart unless-stopped \
+# 		--memory=${NTFY_MEMORY} \
+# 		--cpus=${NTFY_CPUS} \
+# 		--cgroup-parent=/podman-group.slice \
+# 		docker.io/binwiederhier/ntfy:v2.17
--- make/podman.mk
+++ make/podman.mk
@@ -20,6 +20,7 @@ podman-load-images:
 	podman load < ./deployment/images/synapse_1.135.0.tar
 	podman load < ./deployment/images/teddysun_xray-25.10.15.tar
 	podman load < ./deployment/images/satishweb-doh-server_v2.3.10-alpine.tar
+	podman load < ./deployment/images/certbot_5.3.1.tar
 
 podman-info:
 	podman ps -w 1
