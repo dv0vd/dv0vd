7c1cff020addf643418c6d375444590d3c9db7b2
Viacheslav Davydov <dv0vd@dv0vd.xyz>
2026-02-15 18:34:19 +0300

    Revert "Letsencrypt certificates for nginx"
    This reverts commit 5d49884067fd3e5d4e5ce26fee919689408ad05a.

--- make/start.mk
+++ make/start.mk
@@ -135,8 +135,7 @@ start-nginx:
 	-v ./demo:/demo:ro \
 	-v ./src:/app:ro \
 	-v ./deployment/configs/pihole:/app/pihole:ro \
-	-v ./deployment/data/letsencrypt/acme:/app/letsencrypt/acme:ro \
-	-v ./deployment/data/letsencrypt/data/live:/app/letsencrypt/certificates:ro \
+	-v ./deployment/data/letsencrypt/acme:/app/letsencrypt:ro \
 	-p 80:80 \
 	-p 443:443 \
 	-p 8448:8448 \
