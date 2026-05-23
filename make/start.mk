6585f93105f1a655995f0c6a5efd135c1dd98adc
Viacheslav Davydov <viacheslav_davydov@dv0vd.dev>
2026-05-23 18:52:51 +0300

    fix(whitelist): fixed wb

--- make/start.mk
+++ make/start.mk
@@ -509,7 +509,7 @@ start-whitelist-bypass:
 		-e VK_TOKEN=${WHITELIST_BYPASS_VK_TOKEN} \
 		-e VK_GROUP_ID=${WHITELIST_BYPASS_VK_GROUP_ID} \
 		-e TM_COOKIES=/app/cookies-telemost.json \
-		-e TM_COOKIES=/app/cookies-telemost.json \
+		-e WB_COOKIES=/app/cookies-wbstream.json \
 		-e VK_COOKIES=/app/cookies-vk.json \
 		-v ./deployment/configs/whitelist-bypass:/app \
 		--network podman_network \
