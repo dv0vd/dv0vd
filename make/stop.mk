815287af2ad2dacd94e9acc340a684a906bd811b
Viacheslav Davydov <viacheslav_davydov@dv0vd.dev>
2026-08-20 21:00:34 +0300

    chore: merge conflicts

--- make/stop.mk
+++ make/stop.mk
@@ -27,9 +27,6 @@ stop-outline:
 	- podman stop outline
 	- podman rm outline
 
-stop-xray-vless-reality-whitelist-bypass:
-	- podman stop xray-vless-reality-whitelist-bypass
-	- podman rm xray-vless-reality-whitelist-bypass
 
 stop-xray-vless-reality:
 	- podman stop xray-vless-reality
